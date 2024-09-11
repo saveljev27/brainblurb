2b6a87e93657c7c1440c5728ea338902b1e1717d
saveljev27 <141144801+saveljev27@users.noreply.github.com>
2024-09-11 15:10:06 +0300

    header fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ import clouds from '/public/assets/clouds.svg';
 export default function Header() {
   return (
     <header className="flex relative flex-col items-center px-16 pt-6 pb-36 w-full min-h-[300px] max-md:min-h-[200px] max-md:px-5 max-md:pb-12 max-md:max-w-full overflow-visible">
-      <div className="absolute inset-0 w-full h-[380px] max-md:h-[280px] overflow-visible translate-y-[-100px]">
+      <div className="absolute inset-0 w-full h-[420px] max-md:h-[220px] overflow-visible md:translate-y-[-120px]">
         <Image
           loading="lazy"
           src={clouds}
